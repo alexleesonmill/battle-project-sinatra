a3cace177bf4efdcb0ca1a92aa8b536ed6089904
Alex Leeson-Mill <alexleesonmill@gmail.com>
2020-11-11 17:33:53 +0000

    Added method to attack another player

--- lib/Player.rb
+++ lib/Player.rb
@@ -1,6 +1,16 @@
 class Player
-  attr_reader :name
-  def initialize(name)
+  HEALTH = 100
+  attr_reader :name, :health
+  def initialize(name, health = HEALTH)
     @name = name
+    @health = health
+  end
+
+  def attack(player)
+    player.deal_damage
+  end
+
+  def deal_damage
+    @health -= 10
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,8 +2,11 @@ require 'Player'
 
 describe Player do
   subject(:alex) { described_class.new('Alex') }
-
-  it 'returns a name' do
-    expect(alex.name).to eq 'Alex'
+  subject(:chris) { described_class.new('Chris') }
+  it 'returns the name given' do
+    expect(chris.name).to eq('Chris')
+  end
+  it 'does 10 damage' do
+    expect { chris.attack(b) }.to change { a.health }.by(-10)
   end
 end
