b33174a260ca8972a2ae62b6242882c2c81d0cef
Alex Leeson-Mill <alexleesonmill@gmail.com>
2020-11-12 14:54:30 +0000

    Updated skinny logic

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,20 @@
 require 'game'
 
 describe Game do
-  subject(:game) { described_class.new }
+  subject(:game) { described_class.new(alex, chris) }
   let(:alex) { double :alex }
   let(:chris) { double :chris }
   it 'deals damage to a player' do
     expect(chris).to receive(:deal_damage)
     game.attack(chris)
+  end
+
+  it 'gets player 1' do
+    expect(game.player_1).to eq alex
+  end
+
+  it 'gets player 2' do
+    expect(game.player_2).to eq chris
   end
 
   #   it 'does 10 damage' do
